579795622d629076bc90fba32d556e916b49aec0
Jack Nelson <jnelson@commnetsystems.com>
2018-03-08 18:11:16 -0800

    adjustments to scheduler

--- prog/scheduler.py
+++ prog/scheduler.py
@@ -90,3 +90,51 @@ def run_auto_schedule(modf, mfgCenters, dateList):
 	    moLinesLabor.at[index, 'NewDate'] = newDate
 
 	return(moLinesLabor.copy())
+
+# create a new schedule based on the current MO dates and available labor by date
+# also consider earliest allowed schedule dates
+def run_auto_schedule_with_limitations(modf, mfgCenters, dateList):
+	# getting a list of Finished Goods on MO's
+	moFgOnly = modf[modf['ORDERTYPE'] == 'Finished Good'].copy()
+
+	# sorting by date so the earliest scheduled can be the highest priority
+	moFgOnly.sort_values('DATESCHEDULED', inplace=True)
+
+	# renaming part column to match MO header
+	mfgCenters.rename(columns={'Part':'PART'}, inplace=True)
+
+	# adding centers and labor estimates to MO lines
+	moLinesLabor = pd.merge(moFgOnly.copy(), mfgCenters.copy(), how='left', on='PART')
+
+	# save missing info for later.  Will want user to see what items were missed for lack of data.
+	missingCenters = moLinesLabor[moLinesLabor['Mfg Center'].isnull()].copy()
+	missingSetup = moLinesLabor[moLinesLabor['Setup'].isnull()].copy()
+	missingLabor = moLinesLabor[moLinesLabor['LaborPer'].isnull()].copy()
+
+	# replace nulls with 0 for maths.  Probably not necessary, didn't test.
+	moLinesLabor.fillna(0, inplace=True)
+
+	# create a column for the total labor required for each order
+	moLinesLabor['LaborRequired'] = moLinesLabor['Setup'] + (moLinesLabor['LaborPer'] * moLinesLabor['QTYREMAINING'])
+
+	# calculate cumulative labor needed for builds in their current date order
+	moLinesLabor['CumulativeLaborRequired'] = np.nan
+	x = 0
+	for index in moLinesLabor.index:
+	    moLinesLabor.at[index, 'CumulativeLaborRequired'] = moLinesLabor.at[index, 'LaborRequired'].copy() + x
+	    x = moLinesLabor.at[index, 'CumulativeLaborRequired'].copy()
+
+
+	### -----------------------------------
+	# This section compares labor availability to labor needed to create a schedule.
+
+	# add new column with estimated schedule date.  For each row, compare the cumulative labor to the dateList.
+	# if the cumulative labor is less than the total labor available, then set the date.
+	moLinesLabor['NewDate'] = np.nan
+	for index in moLinesLabor.index:
+	    laborNeeded = moLinesLabor.at[index, 'CumulativeLaborRequired'].copy()
+	    tempDateList = dateList[dateList['AvailableLabor'] >= laborNeeded].copy()
+	    newDate = tempDateList['StartDate'].iat[0]
+	    moLinesLabor.at[index, 'NewDate'] = newDate
+
+	return(moLinesLabor.copy())
